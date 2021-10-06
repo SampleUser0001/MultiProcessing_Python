c2238801b8b9054602e315da16c06e9a20e74f15
SampleUser0001 <ittimfn+github@gmail.com>
2021-10-06 11:51:11 +0900

    tmp

--- app/src/app.py
+++ app/src/app.py
@@ -7,6 +7,8 @@ from logutil import LogUtil
 from importenv import ImportEnvKeyEnum
 import importenv as setting
 
+from fork import Fork
+
 PYTHON_APP_HOME = os.getenv('PYTHON_APP_HOME')
 logger = getLogger(__name__)
 log_conf = LogUtil.get_log_conf(PYTHON_APP_HOME + '/config/log_config.json')
@@ -18,13 +20,14 @@ logger.addHandler(handler)
 logger.propagate = False
 
 if __name__ == '__main__':
-  # .envの取得
-  # setting.ENV_DIC[ImportEnvKeyEnum.importenvに書いた値.value]
-  
   # 起動引数の取得
   # args = sys.argv
   # args[0]はpythonのファイル名。
   # 実際の引数はargs[1]から。
+
+  args = sys.argv
   
-  print('Hello Python on Docker!!')
-  logger.info('This is logger message!!')
+  if args[1] == 'fork':
+    Fork.main()
+  else:
+    ValueError('{} is not defined.'.format(args[1]))
